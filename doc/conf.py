bcdc87284fe59624ada48bafc5d6ccccd2b497a0
Simon Knott <info@simonknott.de>
2022-04-27 15:56:39 +0200

    0.0.2

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ copyright = "2022, Simon Knott"  # pylint: disable=redefined-builtin
 author = "Simon Knott"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.0.2"
 
 
 # -- General configuration ---------------------------------------------------
