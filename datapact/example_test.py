8939fa48c172258cf376061c61ab4a662fbe9d0e
Simon Knott <info@simonknott.de>
2022-05-16 11:09:54 +0200

    add dask and pandas example test

--- datapact/example_test.py
+++ datapact/example_test.py
@@ -2,7 +2,17 @@
 tests example
 """
 
+import pytest
+import pandas
+import dask.dataframe
+import dask.delayed
 from datapact.example import add_one
+
+
+def compute(value):
+    if "compute" in dir(value):
+        return value.compute()
+    return value
 
 
 def test_add_one():
@@ -10,3 +20,16 @@ def test_add_one():
     test
     """
     assert add_one(10) == 11
+
+
+iris_pandas = pandas.read_csv("datapact/iris.csv")
+iris_dask = dask.dataframe.read_csv("datapact/iris.csv")
+
+
+@pytest.fixture(params=[iris_pandas, iris_dask])
+def iris_df(request) -> pandas.DataFrame:
+    return request.param
+
+
+def test_fixture(iris_df: pandas.DataFrame):
+    assert compute(iris_df.size) == 750
