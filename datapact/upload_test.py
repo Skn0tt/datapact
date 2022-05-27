4a504449ca7fb9a89e3ccfdd261a23183816f1a6
Simon Knott <info@simonknott.de>
2022-05-27 17:21:20 +0200

    fix: add missing files

---
+++ datapact/upload_test.py
@@ -0,0 +1,24 @@
+# pylint: disable=redefined-outer-name
+
+import pandas
+import pytest_httpserver
+
+import datapact
+from datapact.fixture_test import iris_df  # pylint: disable=unused-import
+
+
+def test_upload(httpserver: pytest_httpserver.HTTPServer, iris_df: pandas.DataFrame):
+    httpserver.expect_request(r"/api/v1/testruns/.*")
+    api_url = httpserver.url_for("/")
+
+    dp = datapact.test(iris_df)
+    dp.connect(api_url, token="abc")
+    dp.SepalLength.should.be_between(0, 4)
+    dp.check()
+
+
+def test_protocol_default_https(iris_df: pandas.DataFrame):
+    dp = datapact.test(iris_df)
+    dp.connect("track.datapact.dev", "foo")
+    assert dp.url is not None
+    assert dp.url.startswith("https://")
